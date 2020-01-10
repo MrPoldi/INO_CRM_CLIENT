671a963af400360e704c9ac135cf59aa882d5aba
Kamil Guttmann <kamgut@st.amu.edu.pl>
2020-01-10 15:11:33 +0100

    Registration and login

---
+++ INO_CRM_WEB_APP/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using INO_CRM_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INO_CRM_WEB_APP.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public async Task<IActionResult> Login(UserModel user)
+        {
+            string token ="Bad token";            
+            string apiUrl = "http://localhost:50060/";
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(apiUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/auth/token", user);
+            if (response.IsSuccessStatusCode)
+            {
+                token = response.Content.ReadAsStringAsync().Result;
+            }
+            token = token.Remove(token.Length - 1);
+            token = token.Substring(1);
+            //string uToken = Regex.Unescape(token);
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken = tokenHandler.ReadJwtToken(token);
+
+
+            return Content($"Hello {user.Login} {user.Password} {token} {jwtToken.Issuer} {jwtToken.Audiences.ToArray()[0]}");
+        }
+    }
+}
---
+++ INO_CRM_WEB_APP/Controllers/RegisterController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using INO_CRM_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INO_CRM_WEB_APP.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register(UserModel user)
+        {            
+            string apiUrl = "http://localhost:50060/";
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(apiUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/users", user);
+            if (response.IsSuccessStatusCode)
+            {
+                
+            } 
+
+            return Content($"Registered user: {user.Login} with pass: {user.Password}");
+        }
+    }
+}
